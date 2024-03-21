60210af8930b5cad4921ac1ea12dd08d5f5c7c71
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-21 15:49:04 -0300

    third exercise

---
+++ X3.py
@@ -0,0 +1,37 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
+def twoSum(nums, target):
+    result = [None, None]
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j:
+                sum = nums[i] + nums[j]
+
+                if (sum == target):
+                    result = [j, i]
+    return result
+                
+examples = [[[2,7,11,15],9], [[3,2,4], 6],[[3,3], 6]]
+
+for e in examples:
+    print(twoSum(e[0], e[1]))
